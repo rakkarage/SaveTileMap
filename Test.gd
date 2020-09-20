1c39494f31fb8d4ba0d0f123bd93166bf8b6a097
Rakka Rage <rakkarage@gmail.com>
2020-09-20 11:54:40 -0400

    x

--- Test.gd
+++ Test.gd
@@ -13,16 +13,20 @@ func _ready() -> void:
 	assert(_saveButton.connect("pressed", self, "_save") == OK)
 	assert(_clearButton.connect("pressed", self, "_clear") == OK)
 	assert(_loadButton.connect("pressed", self, "_load") == OK)
+	_sprite.modulate = Color.red
 
 func _save() -> void:
+	print("save")
 	var packed := PackedScene.new()
 	assert(packed.pack(_persist) == OK)
 	assert(ResourceSaver.save(_path, packed) == OK)
 
 func _clear() -> void:
+	print("clear")
 	_back.clear()
 	_fore.clear()
 
 func _load() -> void:
+	print("load")
 	var packed: PackedScene = ResourceLoader.load(_path)
 	_persist = packed.instance()
