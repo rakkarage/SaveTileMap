dbe3a491f01fa163036c08b3061b3d93a69179de
Rakka Rage <rakkarage@gmail.com>
2020-09-21 11:37:37 -0400

    x

--- Test.gd
+++ Test.gd
@@ -1,32 +1,39 @@
 extends Control
 
 onready var _parent := $Parent
-onready var _saveButton: Button = $Interface/Panel/VBox/Save
-onready var _clearButton: Button = $Interface/Panel/VBox/Clear
-onready var _loadButton: Button = $Interface/Panel/VBox/Load
-const _path := "res://Saved.tscn"
+onready var _saveButton0: Button = $Interface/Panel/HBox/VBox0/Save
+onready var _clearButton0: Button = $Interface/Panel/HBox/VBox0/Clear
+onready var _loadButton0: Button = $Interface/Panel/HBox/VBox0/Load
+onready var _saveButton1: Button = $Interface/Panel/HBox/VBox1/Save
+onready var _clearButton1: Button = $Interface/Panel/HBox/VBox1/Clear
+onready var _loadButton1: Button = $Interface/Panel/HBox/VBox1/Load
+const _path0 := "res://Saved.tscn"
+const _path1 := "res://Saved.cfg"
 
 func _ready() -> void:
 	$Parent/Back.owner = _parent
 	$Parent/Fore.owner = _parent
-	assert(_saveButton.connect("pressed", self, "_save") == OK)
-	assert(_clearButton.connect("pressed", self, "_clear") == OK)
-	assert(_loadButton.connect("pressed", self, "_load") == OK)
-
-func _save() -> void:
-	print("save")
-	var packed := PackedScene.new()
-	assert(packed.pack(_parent) == OK)
-	assert(ResourceSaver.save(_path, packed) == OK)
+	assert(_saveButton0.connect("pressed", self, "_save0") == OK)
+	assert(_clearButton0.connect("pressed", self, "_clear") == OK)
+	assert(_loadButton0.connect("pressed", self, "_load0") == OK)
+	assert(_saveButton1.connect("pressed", self, "_save1") == OK)
+	assert(_clearButton1.connect("pressed", self, "_clear") == OK)
+	assert(_loadButton1.connect("pressed", self, "_load1") == OK)
 
 func _clear() -> void:
 	print("clear")
 	$Parent/Back.clear()
 	$Parent/Fore.clear()
 
-func _load() -> void:
-	print("load")
-	var packed: PackedScene = ResourceLoader.load(_path)
+func _save0() -> void:
+	print("save0")
+	var packed := PackedScene.new()
+	assert(packed.pack(_parent) == OK)
+	assert(ResourceSaver.save(_path0, packed) == OK)
+
+func _load0() -> void:
+	print("load0")
+	var packed: PackedScene = ResourceLoader.load(_path0)
 	var scene = packed.instance()
 	for child in scene.get_children():
 		var exist = _parent.get_node(child.name)
@@ -35,3 +42,9 @@ func _load() -> void:
 			scene.remove_child(child)
 			_parent.add_child(child, true)
 			child.owner = _parent
+
+func _save1() -> void:
+	print("save1")
+
+func _load1() -> void:
+	print("load1")
