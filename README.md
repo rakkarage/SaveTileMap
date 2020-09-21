09427e3bd7bc877568ef0a8c23c19f17115c3e57
Rakka Rage <rakkarage@gmail.com>
2020-09-20 20:29:22 -0400

    x

--- README.md
+++ README.md
@@ -2,15 +2,9 @@
 
 - save: ok but errors
     is_editable_instance: Condition "!is_a_parent_of(p_node)" is true. Returned: false
-- load: how to merge scene
-- better to just save a list of id and coords and flips myself?
-  - how does tile_data combine those three into one int array?
 
 ## Todo
 
-- fix merge
-  - by not having sprite have same parent?
-  - with loop?
 - fix error!?
 - fuck it all and go back to saving in json
   - make test using json too
--- SaveTileMap/Test0.gd
+++ SaveTileMap/Test0.gd
@@ -1,43 +1,37 @@
 extends Control
 
 onready var _parent := $Parent
-onready var _back: TileMap = $Parent/Back
-onready var _sprite: Sprite = $Parent/Sprite
-onready var _fore: TileMap = $Parent/Fore
 onready var _saveButton: Button = $Interface/Panel/VBox/Save
 onready var _clearButton: Button = $Interface/Panel/VBox/Clear
 onready var _loadButton: Button = $Interface/Panel/VBox/Load
 const _path := "res://Saved.tscn"
 
 func _ready() -> void:
-	_back.owner = _persist
-	_fore.owner = _persist
+	$Parent/Back.owner = _parent
+	$Parent/Fore.owner = _parent
 	assert(_saveButton.connect("pressed", self, "_save") == OK)
 	assert(_clearButton.connect("pressed", self, "_clear") == OK)
 	assert(_loadButton.connect("pressed", self, "_load") == OK)
-	_sprite.modulate = Color.red
 
 func _save() -> void:
 	print("save")
 	var packed := PackedScene.new()
-	assert(packed.pack(_persist) == OK)
+	assert(packed.pack(_parent) == OK)
 	assert(ResourceSaver.save(_path, packed) == OK)
 
 func _clear() -> void:
 	print("clear")
-	_back.clear()
-	_fore.clear()
+	$Parent/Back.clear()
+	$Parent/Fore.clear()
 
 func _load() -> void:
 	print("load")
 	var packed: PackedScene = ResourceLoader.load(_path)
 	var scene = packed.instance()
 	for child in scene.get_children():
-		print(child.name)
-		var exist = _persist.get_node(child.name)
-		print(exist)
+		var exist = _parent.get_node(child.name)
 		if exist != null:
-			exist.queue_free()
-			scene.rem
-			_persist.add_child(child)
-			child.owner = _persist
+			exist.free()
+			scene.remove_child(child)
+			_parent.add_child(child, true)
+			child.owner = _parent
